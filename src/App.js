dab3f5a1fe497ca4d4063800d61e7b9aa13ca691
Oliver R <56262527+orreinert@users.noreply.github.com>
2021-02-02 22:03:26 +0100

    worked on

--- src/App.js
+++ src/App.js
@@ -1,45 +1,64 @@
-import React from 'react';
-import MomentUtils from '@date-io/moment';
+import React, { useState } from 'react';
+import moment from '@date-io/moment';
 import {
   TimePicker,
   MuiPickersUtilsProvider,
 } from '@material-ui/pickers';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedBeginTime: new Date(),
-      selectedWorkTime: new Date(),
-      selectedEndTime: new Date(),
+function App() {
+  const [beginTime, setBegin] = useState(new Date());
+  const [workTime, setWork] = useState(new Date());
+  const [endTime, setEnd] = useState(new Date());
+
+  function calculateEnd() {
+    let breakMinutes = 0;
+    console.log(beginTime);
+    console.log(workTime);
+
+    let hours = workTime.getHours();
+    let minutes = workTime.getMinutes();
+
+    if (hours < 6 || (hours === 6 && minutes === 0)) {
+      breakMinutes = 0;
     }
-    this.state.selectedWorkTime.setHours(8, 0, 0, 0);
+
+    if ((hours === 6 && minutes >= 1) || hours > 6) {
+      breakMinutes = 30;
+
+      if (hours >= 9 && minutes >= 1) {
+        breakMinutes += 15;
+      }
+    }
+
+    let end = beginTime;
+    end = moment(end).add(workTime.getHours(), 'hours');
+    end = moment(end).add(workTime.getMinutes(), 'minutes');
+    end = moment(end).add(breakMinutes, 'minutes');
+    setEnd(end);
   }
 
-  handleChange(e) {
-    console.log(e);
+  function handleBeginChange(event) {
+    let time = new Date(event);
+    console.log(time);
+    setBegin(time);
+    calculateEnd();
   }
 
-  render() {
-    return (
-      <div>
-        <p>Arbeitsbeginn:</p>
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <TimePicker value={this.state.selectedBeginTime} onChange={this.handleChange} ampm={false} />
-        </MuiPickersUtilsProvider>
-
-        <p>Arbeitszeit:</p>
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <TimePicker value={this.state.selectedWorkTime} onChange={this.handleChange} ampm={false} />
-        </MuiPickersUtilsProvider>
-
-        <p>Arbeitsende:</p>
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <TimePicker value={this.state.selectedEndTime} ampm={false} readOnly={true} />
-        </MuiPickersUtilsProvider>
-      </div >
-    );
+  function handleWorkChange(event) {
+    setWork(new Date(event));
+    calculateEnd();
   }
+
+  return (
+    <MuiPickersUtilsProvider utils={moment}>
+      <p>Arbeitsbeginn:</p>
+      <TimePicker value={beginTime} onChange={handleBeginChange} ampm={false} />
+      <p>Arbeitszeit:</p>
+      <TimePicker value={workTime} onChange={handleWorkChange} ampm={false} />
+      <p>Arbeitsende:</p>
+      <TimePicker value={endTime} ampm={false} readOnly={true} />
+    </MuiPickersUtilsProvider>
+  );
 }
 
 export default App;
