71bc065851ea9456707172079d6198f422db7039
o.reinert <o.reinert@onlineprinters.com>
2021-02-02 16:33:22 +0100

    added logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import MomentUtils from '@date-io/moment';
 import {
   TimePicker,
   MuiPickersUtilsProvider,
 } from '@material-ui/pickers';
 
-function App() {
-  const [selectedDate, handleDateChange] = useState(new Date());
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedBeginTime: new Date(),
+      selectedWorkTime: new Date(),
+      selectedEndTime: new Date(),
+    }
+    this.state.selectedWorkTime.setHours(8, 0, 0, 0);
+  }
 
-  return (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-      <TimePicker value={selectedDate} onChange={handleDateChange} />
-    </MuiPickersUtilsProvider>
-  );
+  handleChange(e) {
+    console.log(e);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Arbeitsbeginn:</p>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <TimePicker value={this.state.selectedBeginTime} onChange={this.handleChange} ampm={false} />
+        </MuiPickersUtilsProvider>
+
+        <p>Arbeitszeit:</p>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <TimePicker value={this.state.selectedWorkTime} onChange={this.handleChange} ampm={false} />
+        </MuiPickersUtilsProvider>
+
+        <p>Arbeitsende:</p>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <TimePicker value={this.state.selectedEndTime} ampm={false} readOnly={true} />
+        </MuiPickersUtilsProvider>
+      </div >
+    );
+  }
 }
+
 export default App;
