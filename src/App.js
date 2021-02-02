06dd932bf54be1ff3ff4b6b9e6634b869e094ecd
o.reinert <o.reinert@onlineprinters.com>
2021-02-02 15:11:58 +0100

    time picker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import MomentUtils from '@date-io/moment';
+import {
+  TimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
 
 function App() {
+  const [selectedDate, handleDateChange] = useState(new Date());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <TimePicker value={selectedDate} onChange={handleDateChange} />
+    </MuiPickersUtilsProvider>
   );
 }
-
 export default App;
